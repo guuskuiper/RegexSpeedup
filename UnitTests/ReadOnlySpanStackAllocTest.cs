dfba14a15ab34652a6398c2e08aa7a1029b83d85
guuskuiper <guuskuiper@gmail.com>
2021-11-28 09:14:02 +0100

    Add stackalloc tests

---
+++ UnitTests/ReadOnlySpanStackAllocTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegexSpeedup;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ReadOnlySpanStackAllocTest : RemoveWhiteSpaceBaseTest
+    {
+        protected override string RemoveAdditionalWhiteSpace(string input)
+        {
+            return RemoveAdditionalWhiteSpaceSpan.ReadOnlySpanBuffer(input.AsSpan());
+        }
+    }
+}
---
+++ UnitTests/SpanStackAllocTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegexSpeedup;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SpanStackAllocTest : RemoveWhiteSpaceBaseTest
+    {
+        protected override string RemoveAdditionalWhiteSpace(string input)
+        {
+            unsafe
+            {
+                var chars = stackalloc char[input.Length];
+                Span<char> span = new Span<char>(chars, input.Length);
+                input.AsSpan().CopyTo(span);
+                int count = RemoveAdditionalWhiteSpaceSpan.ReplaceWithSingleWhiteSpaceAllocFree(span);
+                return new string(chars, 0, count);
+            }
+        }
+    }
+}
